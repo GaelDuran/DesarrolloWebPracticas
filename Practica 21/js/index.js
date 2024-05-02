4ff96f5a89bcfe856a7a7e7bf198745d008dbb29
GaelDuran <ldt76578@lasallebajio.edu.mx>
2024-05-02 08:13:39 -0600

    Practica 21

--- Practica 21/js/index.js
+++ Practica 21/js/index.js
@@ -15,6 +15,8 @@
         Alumnos 301
         ESTA SEMANA COMIENZAN los examenes del segundo parcial
         Suerte!
+
+        Alt + 96 apostrofe invertido o backtick
     `;
 
     console.log(text);
@@ -32,4 +34,53 @@
     `;
     console.log(resultado);
     lista.innerHTML = resultado;
+
+    let persona = 'Pablo';
+    let edad = 16;
+    let cadena = 'my name is';
+
+    function miFuncion(cadena, personaX, edadExp){
+        let resultado = ``;
+
+        //resultado += cadena + personaX + 'tiene una edad de' + edadExp + 'años';
+
+        resultado = `${cadena} ${personaX} tiene una edad de ${edadExp} años`;
+
+        console.log(resultado);
+        return resultado;
+    }
+
+    miFuncion("Nombre:",persona,edad);
+    
+    //let resultado2 = miFuncion`Hello ${cadena} ${persona} your age is ${edad} years`;
+    //console.log(resultado2);
+
+    function mostrarNombre(name){
+        console.log(name);
+        return name;
+    }
+
+    let resultado2 = mostrarNombre` Raul `;
+
+    let titulo = "Ganadores de los Oscares 2024";
+    let cantantes = ["Billie Elish","Lady Gaga","Adele"];
+    let cadenaHTML = `<h1>${titulo}</h1>`;
+
+    cadenaHTML += `<ul>`;
+
+    for(let cantante of cantantes){
+        cadenaHTML += `<li>${cantante}</li>`;
+    }
+
+    cadenaHTML += `</ul>`;
+    
+    lista.innerHTML = cadenaHTML;
+
+    x = 3,14;
+    otraFuncion();
+
+    function otraFuncion(){
+        "use strict";
+        let y = 3.14;
+    }
 
